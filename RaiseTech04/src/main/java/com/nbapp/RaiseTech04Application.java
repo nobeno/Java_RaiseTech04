0aac9a5d7db5e6fb927cd5348c449ead1c1f58f6
daichi nb <nd940501@gmail.com>
2019-12-20 22:00:16 +0900

    Tomcatデプロイのための設定追加

--- RaiseTech04/src/main/java/com/nbapp/RaiseTech04Application.java
+++ RaiseTech04/src/main/java/com/nbapp/RaiseTech04Application.java
@@ -2,12 +2,19 @@ package com.nbapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RaiseTech04Application {
+public class RaiseTech04Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RaiseTech04Application.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RaiseTech04Application.class);
+    }
 
 }
